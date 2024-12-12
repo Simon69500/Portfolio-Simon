64b88ff622dd38c3dd506a101fe8b7782fae8123
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2024-12-12 23:16:07 +0100

    delete react router

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import React from 'react';
 
 import Header from './Component/Header'; // Exemple d'autre composant
 import Home from './Pages/Home'; // Exemple d'autre composant
-import { Routes, Route } from 'react-router-dom';  // Importer Route et Routes de react-router-dom
-import Competences from './sous-pages/Competences.jsx';
-import About from './sous-pages/About.jsx';
 
 function App() {
   
 
   return (
         <div>
-          <Header /> {/* Autres composants */}
-
-          <Routes> {/* Définir les routes ici */}
-            <Route path="/" element={<Home />} /> {/* Route vers la page d'accueil */}
-            <Route path="/about" element={<About />} /> {/* Route vers la page About */}
-            <Route path="/competences" element={<Competences />} /> {/* Route vers la page Competences */}
-            <Route path="*" element={<div>Page not found</div>} />
-          </Routes>
-
+          <Header /> 
+          <Home />
         </div>
   );
 }
