5030efa194be0877814adc1dbc529e3a61645ced
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-10-01 14:25:37 +0200

    feat(router): install react-router-dom and setup Header routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './Component/Pages/Home';
+import Footer from './Component/layouts/Footer';
+import Header from './Component/layouts/Header.jsx';
+import Competences from './Component/Pages/Competences.jsx';
+import Portfolio from './Component/Pages/Portfolio.jsx';
+import Formulaire from './Component/Pages/Formulaire.jsx';
 
 function App() {
   
 
   return (
-        <div>
-          <Home />
-        </div>
+          <>
+                <Header />
+
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/competences" element={<Competences />} />
+                  <Route path="/portfolio" element={<Portfolio />} />
+                  <Route path="/contact" element={<Formulaire />} />
+                </Routes>
+                
+                <Footer />
+              </>
   );
 }
 
--- src/Component/layouts/Header.jsx
+++ src/Component/layouts/Header.jsx
@@ -1,77 +1,55 @@
 import '@scss/index.scss';
-import * as React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import { createTheme, ThemeProvider } from '@mui/material/styles';  // Import du ThemeProvider et createTheme
-
-import BurgerMenu from './BurgerMenu';
 import { Icones } from "../ui/Icones";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    const [value, setValue] = React.useState('one');
-    
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  return (
+    <header className="header background-header">
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <div className="container-fluid">
+          {/* Logo à gauche */}
+          <Link className="navbar-brand" to="/">
+            <img
+              src={Icones.profil}
+              alt="Simon Badin"
+              className="rounded-circle ms-5"
+            />
+          </Link>
 
-    // Créer le thème personnalisé
-    const theme = createTheme({
-        components: {
-            MuiTabs: {
-                styleOverrides: {
-                    indicator: {
-                        backgroundColor: '#F3F3F3', // Couleur de l'indicateur
-                    },
-                },
-            },
-            MuiTab: {
-                styleOverrides: {
-                    root: {
-                        color: '#9F9F9F', // Couleur des tabs non sélectionnés
-                        fontSize: 'medium',
-                        fontWeight: '600',
-                        '&.Mui-selected': {
-                            color: '#F3F3F3', // Couleur des tabs sélectionnés
-                        },
-                    },
-                },
-            },
-        },
-    });
+          {/* Burger menu pour mobile */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-    return (
-        <>
-            <div className="header">
-                <Stack direction="row" spacing={2} className="icon-header">
-                    <Avatar src={Icones.profil} alt="Simon Badin" sx={{ width: 56, height: 56 }} />
-                </Stack>
-
-                <Box className="box-menu">
-                    {/* Envelopper les Tabs avec ThemeProvider */}
-                    <ThemeProvider theme={theme}>
-                        <Tabs
-                            value={value}
-                            onChange={handleChange}
-                            aria-label="menu navigation"
-                        >
-                            <Tab value="one" label="Accueil" href="#main" />
-                            <Tab value="two" label="A Propos" href="#about" />
-                            <Tab value="three" label="Mes Compétences" href="#Competences" />
-                            <Tab value="four" label="Mon Portfolio" href="#Portfolio" />
-                            <Tab value="five" label="Me Contacter" href="#Contact" />
-                        </Tabs>
-                    </ThemeProvider>
-                </Box>
-
-                <div className="burger-menu">
-                    <BurgerMenu />
-                </div>
-            </div>
-        </>
-    );
+          {/* Liens de navigation */}
+          <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul className="nav nav-underline">
+              <li className="nav-item me-5 fs-4">
+                <Link className="nav-link" aria-current="page"  to="/">Accueil</Link>
+              </li>
+              <li className="nav-item me-5 fs-4">
+                <Link className="nav-link" to="/competences">compétences</Link>
+              </li>
+              <li className="nav-item me-5 fs-4">
+                <Link className="nav-link" to="/portfolio">Portfolio</Link>
+              </li>
+              <li className="nav-item me-5 fs-4">
+                <Link className="btn btn-primary fs-4" role="button" to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
