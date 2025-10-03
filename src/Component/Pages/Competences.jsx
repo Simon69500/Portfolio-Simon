46a36a8a9ceeeb74ab80d7298cdcc4538c4f7af6
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-10-03 17:40:03 +0200

    refactor(skills): redesign page, add About section and soft skills

--- src/Component/Pages/Competences.jsx
+++ src/Component/Pages/Competences.jsx
@@ -20,68 +20,92 @@ const Competences = () => {
         left: 0,
         whiteSpace: 'nowrap',
         width: 1,
-      });
+    });
 
-    return(
-        <>
-        <div id="Competences">
-        <h2 className="title_divers">Mes compétences</h2>
-            <div className="card-cpts">
+    return (
 
-                {/* Partie Présentation (About) */}
-                <About/>
+<div className="Competences">
+    <h2 className="title text-center fs-2 py-4">Mes compétences</h2>
 
-                {/* Partie Skills */}
-                <Skills/>
-            
-                {/* Partie CV */}
-                <div className="contenair-cv">
-                    <h3 className="cv-title">Mon CV</h3>
-                    <a href="https://drive.google.com/file/d/1WezKAjfjamqCfPSN4Ke2mGqWt14Gz5HU/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
-                        <img className="cv-img" src={CV} alt="présentation du CV" />
-                    </a>
+    {/* Partie About full width */}
+        <About />
 
-                    <Box 
-                         sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'space-around', 
-                            alignItems: 'center', 
-                        }}
-                    >
-                    <Button className="Button-Competence"
-                        component="a"
-                        role={undefined}
-                        variant="contained"
-                        tabIndex={-1}
-                        startIcon={<CloudUploadIcon />}
-                        sx={{
-                            background: "#5D5D5D",
-                            color: "#D1D1D1",
-                            margin: '10px',  
-                            width: '150px',
-                            borderRadius: '50px',
-                            border:" solid , 1px ,#8F8F8F"                       
-                        }}
-                        href="https://drive.google.com/file/d/1WezKAjfjamqCfPSN4Ke2mGqWt14Gz5HU/view?usp=drive_link"
-                        download="CV-BADIN-Simon.pdf"
-                        target='_blanck'
-                    >
-                        CV
-                    <VisuallyHiddenInput
-                    type="file"
-                    onChange={(event) => console.log(event.target.files)}
-                    multiple
-                    />
-                    </Button>
-                    </Box>
+    {/* Partie Skills + Soft Skills à gauche et CV à droite */}
+    <div className="d-flex flex-lg-row flex-column justify-content-between align-items-start mt-5">
+        
+        {/* Partie gauche : Skills + Soft Skills */}
+        <div className="left-section w-100 w-lg-50 pe-lg-4 mb-4 ms-5 mb-lg-0">
+            <Skills />
+
+            <div className='soft-skills mt-5'>
+                <h3 className='Subtitle text-center p-3'>Soft Skills</h3>
+                <div className='row'>
+
+                    {[
+                        "Autodidacte",
+                        "Autonomie",
+                        "Gestion du stress",
+                        "Capacité d'adaptation",
+                        "Leadership & Communication",
+                        "Gestion de projet et respect des délais"
+                    ].map((skill, index) => (
+
+                        <div key={index} className='col-6 d-flex align-items-center mb-3 ps-5'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-record-circle-fill" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-8 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                            </svg>
+                            <p className='text ps-2 mb-0'>{skill}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
-                        
-        </div>       
-        </>
-             
+        </div>
+
+
+        {/* Partie droite : CV */}
+        <div className="right-section w-75 w-lg-50 ps-lg-4 text-center">
+            <h3 className="cv-title mb-3">Mon CV</h3>
+            <a href="https://drive.google.com/file/d/1WezKAjfjamqCfPSN4Ke2mGqWt14Gz5HU/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                <img className="cv-img mb-3" src={CV} alt="présentation du CV" />
+            </a>
+
+            <Box 
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center', 
+                    alignItems: 'center', 
+                }}
+            >
+                <Button 
+                    className="Button-Competence"
+                    component="a"
+                    variant="contained"
+                    startIcon={<CloudUploadIcon />}
+                    sx={{
+                        background: "#5D5D5D",
+                        color: "#D1D1D1",
+                        margin: '10px',  
+                        width: '150px',
+                        borderRadius: '50px',
+                        border:"1px solid #8F8F8F"                      
+                    }}
+                    href="https://drive.google.com/file/d/1WezKAjfjamqCfPSN4Ke2mGqWt14Gz5HU/view?usp=drive_link"
+                    download="CV-BADIN-Simon.pdf"
+                    target='_blank'
+                >
+                    CV
+                    <VisuallyHiddenInput
+                        type="file"
+                        onChange={(event) => console.log(event.target.files)}
+                        multiple
+                    />
+                </Button>
+            </Box>
+        </div>
+
+    </div>
+</div>
     )
 };
 
-export default Competences ;
+export default Competences;
--- src/Component/layouts/Skills.jsx
+++ src/Component/layouts/Skills.jsx
@@ -4,9 +4,9 @@ import { Icones } from "../ui/Icones";
 function SkillBar({ skill }) {
     return (
         <div className="skill-bar">
-            <div className="card-logo">
+            <div className="m-2">
                 <img className="skill-img" src={skill.img} alt={skill.name} />
-                <div className="skill-name">{skill.name}</div>
+                <div className="text text-center">{skill.name}</div>
             </div>
         </div>
     );
@@ -43,15 +43,13 @@ function Skills() {
     ];
 
     return (
-        <div className="contenair-skills">
-            <div className="skill-card-title">
-                <h3 className="skill-title-1">Skills</h3>
-            </div>
+        <div className="container d-flex flex-column align-items-center justify-content-evenly ">
+            <h3 className="Subtitle text-center p-3"> Hard Skills</h3>
             
             {/* Frontend */}
-            <div className="skill-category">
-                <h4 className="skill-title-2">Frontend</h4>
-                <div className="skill-card-logo">
+            <div className="w-100 m-2">
+                <h4 className="Subtitle fs-4">Frontend</h4>
+                <div className="d-flex flex-row justify-content-evenly flex-wrap">
                     {skillsFront.map((skill, index) => (
                         <SkillBar key={index} skill={skill} />
                     ))}
@@ -59,9 +57,9 @@ function Skills() {
             </div>
             
             {/* Backend */}
-            <div className="skill-category">
-                <h4 className="skill-title-2">Backend</h4>
-                <div className="skill-card-logo">
+            <div className="w-100 m-2">
+                <h4 className="Subtitle fs-4">Backend</h4>
+                <div className="d-flex flex-row justify-content-evenly flex-wrap">
                     {skillsBackend.map((skill, index) => (
                         <SkillBar key={index} skill={skill} />
                     ))}
@@ -69,9 +67,9 @@ function Skills() {
             </div>
 
             {/* Outils et Plateformes */}
-            <div className="skill-category">
-                <h4 className="skill-title-2">Outils et Plateformes</h4>
-                <div className="skill-card-logo">
+            <div className="w-100 m-2">
+                <h4 className="Subtitle fs-4">Outils et Plateformes</h4>
+                <div className="d-flex flex-row justify-content-evenly flex-wrap">
                     {skillsTools.map((skill, index) => (
                         <SkillBar key={index} skill={skill} />
                     ))}
