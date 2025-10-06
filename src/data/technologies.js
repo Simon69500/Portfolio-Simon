f14330375bb422574b796264d3d65f8284cf6c92
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-10-06 17:10:42 +0200

    refactor(data): move project and skill data to JSON/JS modules

---
+++ src/data/technologies.js
@@ -0,0 +1,61 @@
+// Front-End
+import css from '@img/img-logo/css3.svg';
+import html from '@img/img-logo/html5.svg';
+import react from '@img/img-logo/react.png';
+import scriptJava from '@img/img-logo/scriptJava.png';
+import typescript from '@img/img-logo/typescript.svg';
+import vue from '@img/img-logo/vuejs.png';
+import angular from '@img/img-logo/angular.png';
+import sass from '@img/img-logo/sass.svg'
+import bootstrap from '@img/img-logo/bootstrap.svg'
+
+
+
+// Back-end
+import nodejs from '@img/img-logo/nodejs.svg';
+import express from '@img/img-logo/express-js.svg';
+import mongodb from '@img/img-logo/MongoDB.svg';
+import mysql from '@img/img-logo/mysql.svg';
+import php from '@img/img-logo/php.svg';
+import symfony from '@img/img-logo/symfony.svg';
+
+//Outils & Plateforme
+import wordpress from '@img//img-logo//wordpress.png';
+import github from '@img/img-logo//github.png';
+import figma from '@img/img-logo/figma.png';
+import openai from '@img/img-logo/open.svg';
+import render from '@img/img-logo/render.svg';
+import alwaysdata from '@img/img-logo/alwaysdata.svg';
+
+
+// Fonction qui appels les icones dans les différentes components 
+export const Technologies = {
+
+    // Frontend
+       HTML: { name: 'HTML', img: html },
+       CSS: { name: 'CSS', img: css },
+       JavaScript: { name: 'JavaScript', img: scriptJava },
+       typeScript: { name: 'TypeScript', img: typescript },
+       VueJS: { name: 'Vue.js', img: vue },
+       ReactJS: { name: 'ReactJS', img: react },
+       Angular: { name: 'Angular', img: angular},
+       SASS: { name: 'Sass', img: sass},
+       Boostrap: { name: 'Bootstrap', img: bootstrap},
+       
+    // Backend
+       NodeJS: { name: 'Node.js', img: nodejs },
+       Express: { name: 'Express', img: express },
+       MongoDB: { name: 'MongoDB', img: mongodb },
+       MySQL: { name: 'MySQL', img: mysql },
+       PHP: { name: 'PHP', img: php },
+       Symfony: { name: 'Symfony', img: symfony },
+       
+    // Outils & Plateformes
+       GitHub: { name: 'GitHub', img: github },
+       OpenAI: { name: 'OpenAI', img: openai },
+       WordPress: { name: 'WordPress', img: wordpress},
+       Figma: { name: 'Figma', img: figma },
+       Render: { name: 'Render', img: render },
+       Alwaysdata: { name: 'Alwaysdata', img: alwaysdata },    
+
+};
