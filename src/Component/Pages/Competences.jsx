512f277bea6213f4548bb81ef8cc4089007feb31
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-10-14 15:52:42 +0200

    refactor(competences): improve responsiveness and layout

--- src/Component/Pages/Competences.jsx
+++ src/Component/Pages/Competences.jsx
@@ -14,15 +14,15 @@ const Competences = () => {
         <About />
 
     {/* Partie Skills + Soft Skills à gauche et CV à droite */}
-    <div className="d-flex flex-lg-row flex-column justify-content-between align-items-start mt-5">
+    <div className="d-flex flex-lg-row flex-column justify-content-lg-between align-items-lg-start mt-5">
         
         {/* Partie gauche : Skills + Soft Skills */}
-        <div className="left-section w-100 w-lg-50 pe-lg-4 mb-4 ms-5 mb-lg-0">
+        <div className="left-section w-lg-50 pe-lg-4 mb-4 mx-md-5 mx-3 mb-lg-0">
             <Skills />
 
-            <div className='soft-skills mt-5'>
-                <h3 className='Subtitle text-center p-3'>Soft Skills</h3>
-                <div className='row'>
+            <div className='soft-skills mt-5 mb-lg-5'>
+                <h3 className='Subtitle text-center p-md-3 p-1'>Soft Skills</h3>
+                <div className='row justify-content-center'>
 
                     {[
                         "Autodidacte",
@@ -33,7 +33,7 @@ const Competences = () => {
                         "Gestion de projet et respect des délais"
                     ].map((skill, index) => (
 
-                        <div key={index} className='col-6 d-flex align-items-center mb-3 ps-5'>
+                        <div key={index} className='col-md-6 col-10 d-flex align-items-center mb-md-3 mb-2 ps-md-5 ps-2'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-record-circle-fill" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-8 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                             </svg>
@@ -46,12 +46,12 @@ const Competences = () => {
 
 
         {/* Partie droite : CV */}
-        <div className="CV card right-section  w-lg-50 ps-lg-4 mx-5 mb-5 d-flex justify-content-center align-items-center text-center">
+        <div className="CV card right-section px-lg-5 mx-md-5 mx-3 mb-5 d-flex justify-content-center align-items-center text-center">
            <img className="cv-img mb-3 pt-4" src={CV} alt="présentation du CV" />
             <div className="card-body">
                 <h5 className="card-title Subtitle">Curriculum vitae</h5>
                 <p className="card-text text">Recherche de stage d'une durée de 350h pour validation d'examen en tant que Développeur Web & Mobil FullStack</p>
-                <a href="/CV-BADIN-Simon.pdf" download class="btn btn-primary m-3 p-2">Télécharger mon CV</a>
+                <a href="/CV-BADIN-Simon.pdf" download class="btn btn-sm btn-primary m-3 p-2">Télécharger mon CV</a>
             </div>
         </div>
 
