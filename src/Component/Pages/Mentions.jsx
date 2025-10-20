253490129de0a37d6be303513adedf29309fdafa
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-10-20 15:34:20 +0200

    chore(mentions): add mentions cookies

--- src/Component/Pages/Mentions.jsx
+++ src/Component/Pages/Mentions.jsx
@@ -11,12 +11,19 @@ const Mentions = () => {
             <br/>Développeur Web & Mobile Fullstack  
             <br/>Email : simonsola67@gmail.com
         </p>
-            <p className="text p-3">
+
+        <p className="text p-3">
                 <strong className="text-strong">Hébergement :</strong><br/>
                 Ce site est hébergé par GitHub Pages, un service de GitHub, Inc.<br/>
                 Siège social : 88 Colin P. Kelly Jr. Street, San Francisco, CA 94107, États-Unis.<br/>
                 Site web : <a className="text text-decoration-none" href="https://pages.github.com/" target="_blank" rel="noopener noreferrer">https://pages.github.com</a>
-            </p>
+        </p>
+
+        <p className="text p-3">
+        <strong className="text-strong"> Cookies et données personnelles :</strong><br/>
+        Ce site utilise Google Analytics pour mesurer l’audience. Les données sont anonymisées et utilisées uniquement à des fins statistiques. Vous pouvez désactiver les cookies via votre navigateur.
+        </p>
+        
         <p className="text fst-italic pb-5">
             © 2024 Simon Badin. Tous droits réservés.
         </p>
