f073a55ca67491353520584252d96e00a7a3604a
Simon69500 <simonsola67@gmail.com>
2026-07-22 16:32:06 +0200

    fix: petit ajustement sur la présentation & projet

--- src/components/bento/Presentation.jsx
+++ src/components/bento/Presentation.jsx
@@ -11,7 +11,7 @@ const Presentation = () => {
 
     <article
       className=" h-full bg-bento-light 
-      dark:bg-bento-dark rounded-2xl p-6 shadow-sm flex flex-col justify-between 
+      dark:bg-bento-dark rounded-2xl p-6 shadow-sm flex flex-col justify-center 
       transition-transform duration-300 hover:scale-[1.02] hover:border hover:border-accent-primary "
       aria-labelledby="presentation-title"
     >
@@ -22,7 +22,7 @@ const Presentation = () => {
         {/*Emplacement Photo de Profil */}
         <div className="w-24 h-24 rounded-full shrink-0 overflow-hidden">
           <img
-          src=""
+          src="/images/Profil2.png"
           alt="Portrait de Simon"  
           className="w-full h-full object-cover"
           loading="lazy"
@@ -31,21 +31,21 @@ const Presentation = () => {
 
         {/* Zone de Titre + Texte */}
         <div>
-          <h1 className="text-2xl font-bold mb-3 text-typography-light dark:text-typography-dark" id="presentation-title">Simon Badin - Développeur Web & Mobile Full-Stack</h1>
-          <p className="text-typography-light dark:text-typography-dark-muted leading-relaxed">
-            Ancien Chasseur Alpin et chef d'équipe dans le BTP, je convertis ma rigueur, ma résilience et ma gestion du stress en lignes de code performantes et structurées.
+          <h1 className="text-2xl font-bold text-typography-light dark:text-typography-dark" id="presentation-title">Simon Badin - Développeur Web & Mobile Full-Stack</h1>
+          <p className="m-8 p-4 text-typography-light dark:text-typography-dark-muted leading-relaxed">
+            Ancien Chasseur Alpin et chef d'équipe BTP reconverti Développeur Web & Mobile Full-Stack (RNCP 5). Je combine résilience, rigueur et leadership pour concevoir des applications robustes de A à Z (comme Estimmo-Savoies.fr). Actuellement en recherche d'un poste en CDI (région lyonnaise ou full télétravail/mobilité), prêt à m'investir dans une équipe technique exigeante.
           </p>
         </div>
       </div>       
 
       {/* Zone d'Actions : Les Boutons */}
-      <div className="mt-8 flex flex-col sm:flex-row gap-4">
+      <div className="mt-8 pt-8 flex flex-col justify-center sm:flex-row gap-4">
 
         <Button href="#contact" variant="primary">
           Me Contacter
         </Button>
 
-        <Button href="/CV-Badin_Simon_DevWeb2.pdf" variant="secondary" rel="noopener noreferrer" target="_blank">
+        <Button href="/CV Badin Simon 2026.pdf" download="CV Badin Simon 2026.pdf" variant="secondary" rel="noopener noreferrer" target="_blank">
           Télécharger mon CV
         </Button>
 
